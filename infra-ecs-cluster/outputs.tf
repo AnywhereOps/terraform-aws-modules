afcb08815b46dbc881e7307d437462b9ca5a8635
Andrew Kemp-Dahlberg <akempdah@gmail.com>
2026-02-19 01:03:57 -0800

    Simplify ecs-cluster outputs with cleaner attribute names
    Expose name, arn, id directly instead of raw module output.

--- infra-ecs-cluster/outputs.tf
+++ infra-ecs-cluster/outputs.tf
@@ -1,3 +1,7 @@
 output "cluster" {
-  value = module.cluster
+  value = {
+    name = module.cluster.cluster_name
+    arn  = module.cluster.cluster_arn
+    id   = module.cluster.cluster_id
+  }
 }
