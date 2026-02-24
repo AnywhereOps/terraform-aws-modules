bc02ff65f356dfc0f462e4c3753fd459afee868a
Andrew Kemp-Dahlberg <akempdah@gmail.com>
2026-02-24 14:13:22 -0800

    feat(ecr-repo): Add repository_url and name outputs

--- ecr-repo/outputs.tf
+++ ecr-repo/outputs.tf
@@ -2,3 +2,13 @@ output "arn" {
   description = "Full ARN of the repository."
   value       = aws_ecr_repository.main.arn
 }
+
+output "repository_url" {
+  description = "URL of the repository."
+  value       = aws_ecr_repository.main.repository_url
+}
+
+output "name" {
+  description = "Name of the repository."
+  value       = aws_ecr_repository.main.name
+}
